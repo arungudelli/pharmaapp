84a5d209de72f83a33e1a95b3f1ff27b3947eb34
zvdas <rodriguezzude@gmail.com>
2022-11-23 16:28:40 +0530

    updates to invoice service & distributor service

--- src/app/services/distributor.service.ts
+++ src/app/services/distributor.service.ts
@@ -1,31 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Distributor } from '../models/distributor';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class DistributorService {
   
   constructor(private http: HttpClient) { }
 
   baseUrl = "https://localhost:44396/api";
-  //baseUrl = "http://localhost:3000/distributor";
 
-  // public getDistributors(): Observable<Distributor[]> {      
   public getDistributors() {      
-    // throw new Error('Method not implemented.');
     return this.http.get<Distributor[]>(this.baseUrl+"/Distributor/get");
   }
 
   saveDistributor(distributor: Distributor) {
     this.http.post(this.baseUrl+"/Distributor/save", distributor).subscribe();
-    // this.http.post(this.baseUrl, distributor).subscribe(
-    //   (response)=>  console.log(response),
-    //   (error)=>     console.log(error),
-    //   ()=>          console.log("completed")
-    // );;
   }
 
   getDistributorById(id: number) {
--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -5,14 +5,27 @@ import { Invoice } from '../models/invoice';
 @Injectable({
   providedIn: 'root'
 })
+
 export class InvoiceService {
 
   constructor(private http: HttpClient) { }
 
   baseUrl = "https://localhost:44396/api";
 
+  getInvoices() {
+    return this.http.get<Invoice[]>(this.baseUrl+"/Invoice/get");
+  }
 
   saveInvoice(invoiceData: Invoice[]) {
     this.http.post(this.baseUrl+"/Invoice/save", invoiceData).subscribe();
   }
+
+  getInvoiceById(id: number) {
+    return this.http.get<Invoice>(this.baseUrl+"/Invoice/get/"+id);
+  }
+
+  updateInvoice(id: number, invoice: Invoice) {
+
+  }
+
 }
