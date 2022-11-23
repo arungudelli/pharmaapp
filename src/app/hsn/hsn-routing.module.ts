f91fea4c3dcfa5245600dfd4b11b4f5bf6b27d88
zvdas <rodriguezzude@gmail.com>
2022-11-23 12:32:04 +0530

    updates to references in hsn module

--- src/app/hsn/hsn-routing.module.ts
+++ src/app/hsn/hsn-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HsnListComponent } from './hsn-list.component';
+import { HsnListComponent } from './hsn-list/hsn-list.component';
 
-const routes: Routes = [{ path: '', component: HsnListComponent }];
+const routes: Routes = [
+  { path: '', component: HsnListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HsnListRoutingModule { }
+
+export class HsnRoutingModule { }
