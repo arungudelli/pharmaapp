0e2d323245a38fa0f5ce3f9be0c3996f167d045e
zvdas <rodriguezzude@gmail.com>
2022-11-24 15:49:06 +0530

    invoiceItems model updated hsn removed

--- src/app/models/invoiceItems.ts
+++ src/app/models/invoiceItems.ts
@@ -1,12 +1,10 @@
 import { Distributor } from "./distributor";
-import { Hsn } from "./hsn";
 import { Item } from "./item";
 
 export interface InvoiceItems {
     id: number;
     item: Item;
     distributor: Distributor;
-    hsn: Hsn;
     pack: string;
     batchNo: string;
     mfgDate: Date;
