44f93da126996b167496ebd248b1645c9889acf7
zvdas <rodriguezzude@gmail.com>
2022-11-29 16:14:38 +0530

    static invoice model added

---
+++ src/app/models/staticInvoiceItems.ts
@@ -0,0 +1,15 @@
+    export interface StaticInvoiceItems {
+    id: number, 
+    name: string, 
+    pack: string, 
+    batchNo: string, 
+    mfgDate: Date, 
+    expDate: Date, 
+    qty: number, 
+    freeItems: number, 
+    mrp: number, 
+    rate: number, 
+    amount: number, 
+    gst: number, 
+    hsnCode: string
+}
