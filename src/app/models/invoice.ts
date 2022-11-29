a0b5e257d1292a964de8f07586a9b8cec6ea59a5
zvdas <rodriguezzude@gmail.com>
2022-11-29 11:13:44 +0530

    models updated

--- src/app/models/invoice.ts
+++ src/app/models/invoice.ts
@@ -1,8 +1,12 @@
+import { Distributor } from "./distributor";
 import { InvoiceItems } from "./invoiceItems";
 
 
 export interface Invoice {
     id: number;
+    invoiceNumber: string;
+    invoiceDate: Date;
+    distributor: Distributor;
     invoiceItems: InvoiceItems;
     amount: number;
     totalDiscount: number;
