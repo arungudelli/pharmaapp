cb05fbc1a922802b705138cec815ca037ab95bb7
zvdas <rodriguezzude@gmail.com>
2022-11-30 17:53:09 +0530

    invoice model updated

--- src/app/models/invoice.ts
+++ src/app/models/invoice.ts
@@ -11,25 +11,4 @@ export interface Invoice {
     amount: number;
     totalDiscount: number;
     actualAmount: number;
-   
-    /*
-    tax: number;
-    unit: string;
-    */
-
-    /*
-    Hsnsac: string;
-    Str: string;
-    Ds:string;
-    Loc: string;
-    ProductDescription : string;
-    Pack: string;
-    Mfg: string;
-    FreeDiscount: number;
-    Mrp: number;
-    TotalDiscountP: number,
-    Taxable: number,
-    GstPercentage: number,
-    GstAmount: number,
-    */
 }
