31ff88566583e200a3d12779cf6e15e16ef5bfac
zvdas <rodriguezzude@gmail.com>
2022-12-12 18:00:39 +0530

    bill components updated

--- src/app/modules/bill/bill-list/bill-list.component.ts
+++ src/app/modules/bill/bill-list/bill-list.component.ts
@@ -1,10 +1,147 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Invoice } from 'src/app/models/invoice';
+import { InvoiceService } from 'src/app/services/invoice.service';
+import { EditBillComponent } from '../edit-bill/edit-bill.component';
 
 @Component({
   selector: 'app-bill-list',
   templateUrl: './bill-list.component.html',
   styleUrls: ['./bill-list.component.css']
 })
-export class BillListComponent {
 
+export class BillListComponent {
+  bill: Invoice[] = [];
+
+  selectedBill: Invoice = {} as Invoice;
+
+  columnSchema = [
+    {key: 'select', type: '', label: '', object:'', isObject: false, formGroupName: ''},
+    {key: 'id', type: 'number', label: '#', object:'', isObject: false, formGroupName: ''},
+    {key: 'invoiceNumber', type: 'number', label: 'Bill Number', object:'', isObject: false, formGroupName: ''},
+    {key: 'invoiceDate', type: 'text', label: 'Bill Date', object:'', isObject: false, formGroupName: ''},
+    // {key: 'id', type: 'number', label: 'id', object:'distributor', isObject: true, formGroupName: ''},
+    {key: 'name', type: 'text', label: 'Customer Name', object:'distributor', isObject: true, formGroupName: ''},
+    // {key: 'email', type: 'text', label: 'email', object:'distributor', isObject: true, formGroupName: ''},
+    {key: 'phoneNumber', type: 'number', label: 'Phone Number', object:'distributor', isObject: true, formGroupName: ''},
+    // {key: 'gstin ', type: 'text', label: 'gstin', object:'distributor', isObject: true, formGroupName: ''},
+    // {key: 'pan', type: 'text', label: 'pan', object:'distributor', isObject: true, formGroupName: ''},
+    // {key: 'dlno', type: 'text', label: 'dlno', object:'distributor', isObject: true, formGroupName: ''},
+    // {key: 'address', type: 'text', label: 'address', object:'distributor', isObject: true, formGroupName: ''},
+    // {key: 'city', type: 'text', label: 'city', object:'distributor', isObject: true, formGroupName: ''},
+    // {key: 'state', type: 'text', label: 'state', object:'distributor', isObject: true, formGroupName: ''},
+    // {key: 'pinCode', type: 'text', label: 'pinCode', object:'distributor', isObject: true, formGroupName: ''},
+    // {key: 'id', type: 'number', label: 'id', object: 'invoiceItems', isObject: true, formGroupName: ''},
+    // {key: 'id', type: 'number', label: 'id', object:'item', isObject: true, formGroupName: ''},
+    // {key: 'name', type: 'text', label: 'name', object:'item', isObject: true, formGroupName: ''},
+    // {key: 'description', type: 'text', label: 'description', object:'item', isObject: true, formGroupName: ''},
+    // {key: 'id', type: 'number', label: 'id', object:'hsn', isObject: true, formGroupName: ''},
+    // {key: 'hsnCode', type: 'text', label: 'hsnCode', object:'hsn', isObject: true, formGroupName: ''},
+    // {key: 'description', type: 'text', label: 'description', object:'hsn', isObject: true, formGroupName: ''},
+    // {key: 'gstRate', type: 'number', label: 'gstRate', object:'hsn', isObject: true, formGroupName: ''},
+    // {key: 'id', type: 'number', label: 'id', object:'manfacturer', isObject: true, formGroupName: ''},
+    // {key: 'name', type: 'text', label: 'name', object:'manfacturer', isObject: true, formGroupName: ''},
+    // {key: 'pack', type: 'text', label: 'pack', object:'invoiceItems', isObject: true, formGroupName: ''},
+    // {key: 'batchNo', type: 'text', label: 'batchNo', object:'invoiceItems', isObject: true, formGroupName: ''},
+    // {key: 'mfgDate', type: 'date', label: 'Mfg. Date', object:'invoiceItems', isObject: true, formGroupName: ''},
+    // {key: 'expDate', type: 'date', label: 'expDate', object:'invoiceItems', isObject: true, formGroupName: ''},
+    // {key: 'qty', type: 'number', label: 'qty', object:'invoiceItems', isObject: true, formGroupName: ''},
+    // {key: 'freeItems', type: 'number', label: 'freeItems', object:'invoiceItems', isObject: true, formGroupName: ''},
+    // {key: 'discount', type: 'number', label: 'discount', object:'invoiceItems', isObject: true, formGroupName: ''},
+    // {key: 'mrp', type: 'number', label: 'mrp', object:'invoiceItems', isObject: true, formGroupName: ''},
+    // {key: 'rate', type: 'number', label: 'rate', object:'invoiceItems', isObject: true, formGroupName: ''},
+    // {key: 'amount', type: 'number', label: 'Amount', object:'', isObject: false, formGroupName: ''},
+    // {key: 'totalDiscount', type: 'number', label: 'Total Discount', object:'', isObject: false, formGroupName: ''},
+    {key: 'actualAmount', type: 'number', label: 'Total Amount', object:'', isObject: false, formGroupName: ''},
+    {key: 'action', type: '', label: 'Action', object: '', isObject: false, formGroup: ''}
+  ];
+
+  billColumns: string[] = this.columnSchema.map(col => col.key);
+
+  // invoiceColumns: string[] = ['id', 'items', 'amount', 'totalDiscount', 'actualAmount'];
+
+  billDatasource = new MatTableDataSource<Invoice>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(public dialog: MatDialog, public invoiceService: InvoiceService) { }
+
+  ngOnInit(): void {
+    this.getAllBills();
+  }
+
+  ngAfterViewInit(): void {
+    this.billDatasource.paginator = this.paginator;
+  }
+
+  getAllBills() {
+    this.invoiceService.getInvoices();
+    this.invoiceService.allInvoices.subscribe(res=>{
+      // console.log('get invoices: ', res);
+      this.bill = res;
+      this.billDatasource.data = res;
+    });
+  }
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.billDatasource.filter = filterValue.trim().toLowerCase();
+    
+    if (this.billDatasource.paginator) {
+      this.billDatasource.paginator.firstPage();
+    }
+  }
+
+  openBillDialog() {
+    this.dialog.open(
+      EditBillComponent, 
+      {
+        maxWidth: '100vw', 
+        maxHeight: '100vh', 
+        width: '98%', 
+        height: '98%', 
+        panelClass: 'fixActionRow',
+        autoFocus: false
+      }
+    );
+  }
+
+  editSelectedBill(bill: Invoice[]) {
+
+    const index = Object.entries(bill).at(0)?.at(1)?.valueOf();
+    
+    const billRow = this.bill.find(x=>x.id === index);
+
+    const billRows: any[] = [];
+
+    billRow?.invoiceItems.map(x=> {
+      billRows.push({
+        id: x.id,
+        productName: x.item.name,
+        qty: x.qty,
+        batchNo: x.batchNo,
+        discount: x.discount,
+        mrp: x.mrp,
+        mfgDate: x.mfgDate,
+        expDate: x.expDate,
+      })
+    })
+   
+    // console.log(index, billRow, billRows);
+   
+    this.dialog.open(
+      EditBillComponent,
+      {
+        maxWidth: '100vw', 
+        maxHeight: '100vh', 
+        width: '98%', 
+        height: '98%', 
+        panelClass: 'fixActionRow',
+        autoFocus: false,
+        data: { bill , billRows}
+      }
+    );
+  }
 }
--- src/app/modules/bill/edit-bill/edit-bill.component.ts
+++ src/app/modules/bill/edit-bill/edit-bill.component.ts
@@ -1,10 +1,349 @@
-import { Component } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, Inject } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { map, Observable, startWith } from 'rxjs';
+
+import { BillRows } from 'src/app/models/billRows';
+import { Distributor } from 'src/app/models/distributor';
+import { Invoice } from 'src/app/models/invoice';
+import { InvoiceItems } from 'src/app/models/invoiceItems';
+import { Item } from 'src/app/models/item';
+import { PickDateAdapter } from 'src/app/models/pickDateAdapter';
+import { DocGenBill } from 'src/app/pdfmake-docs/doc-gen-bill';
+import { DistributorService } from 'src/app/services/distributor.service';
+import { InvoiceService } from 'src/app/services/invoice.service';
+import { ItemService } from 'src/app/services/item.service';
+
+const datePickerFormat = {
+  parse: {
+      dateInput: {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric'
+      },
+  },
+  display: {
+    dateInput: 'input',
+    monthYearLabel: {
+      year: 'numeric',
+      month: 'numeric',
+    },
+    dateA11yLabel: {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric'
+    },
+    monthYearA11yLabel: {
+      year: 'numeric',
+      month: 'numeric',
+    }
+  }
+}
 
 @Component({
   selector: 'app-edit-bill',
   templateUrl: './edit-bill.component.html',
-  styleUrls: ['./edit-bill.component.css']
+  styleUrls: ['./edit-bill.component.css'],
+  providers: [
+    { provide: DateAdapter, useClass: PickDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: datePickerFormat }
+  ]
 })
-export class EditBillComponent {
 
+export class EditBillComponent {
+  distributors: Distributor[] = [];
+  
+  items: Item[] = [];
+
+  filteredItemOptions?: Observable<string[]>;
+
+  filteredDistributorOptions?: Observable<string[]>;
+
+  ELEMENT_DATA: BillRows[] = [];
+
+  selectedItems: Item[] = [];
+
+  editBillForm = new FormGroup({
+    billRows: new FormArray(this.ELEMENT_DATA.map(val => new FormGroup({
+      id: new FormControl(val.id), 
+      productName: new FormControl(val.productName),
+      qty: new FormControl(val.qty), 
+      batchNo: new FormControl(val.batchNo), 
+      discount: new FormControl(val.discount), 
+      mrp: new FormControl(val.mrp), 
+      mfgDate: new FormControl(val.mfgDate), 
+      expDate: new FormControl(val.expDate), 
+    })))
+  });
+
+  editBillAccountsForm = new FormGroup({
+    id: new FormControl(),
+    invoiceItems: new FormGroup({
+      id: new FormControl(), 
+      item: new FormGroup({
+        id: new FormControl(),
+        name: new FormControl(),
+        description: new FormControl(),
+        hsn: new FormGroup({
+          id: new FormControl(),
+          hsnCode: new FormControl(),
+          description: new FormControl(),
+          gstRate: new FormControl()
+        }),
+        manfacturer: new FormGroup({
+          id: new FormControl(),
+          name: new FormControl()
+        })
+      }),
+      pack: new FormControl(), 
+      batchNo: new FormControl(), 
+      mfgDate: new FormControl(), 
+      expDate: new FormControl(), 
+      qty: new FormControl(), 
+      freeItems: new FormControl(), 
+      mrp: new FormControl(), 
+      rate: new FormControl(), 
+      discount: new FormControl(), 
+    }),
+    distributor: new FormGroup({
+      id: new FormControl(),
+      name: new FormControl(),
+      email: new FormControl(),
+      phoneNumber: new FormControl(),
+      gstin : new FormControl(),
+      pan: new FormControl(),
+      dlno: new FormControl(),
+      address: new FormControl(),
+      city: new FormControl(),
+      state: new FormControl(),
+      pinCode: new FormControl()
+    }),
+    invoiceNumber: new FormControl(''),
+    // invoiceDate: new FormControl(new Date()),
+    invoiceDate: new FormControl(),
+    amount: new FormControl(),
+    totalDiscount: new FormControl(),
+    actualAmount: new FormControl()
+  });
+
+  billDatasource = new MatTableDataSource((this.editBillForm.get('billRows') as FormArray).controls);
+  
+  selection = new SelectionModel<BillRows>(true, []);
+
+  billColumns: string[] = ['select','id','productName','qty','batchNo','discount','mrp','mfgDate','expDate'];
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {bill: Invoice, billRows: BillRows[]}, public invoiceService: InvoiceService, public distributorService: DistributorService, public itemsService: ItemService, public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.editBillAccountsForm.controls.id.setValue(0);
+    this.editBillAccountsForm.controls.invoiceNumber.setValue('');
+    // this.editBillAccountsForm.controls.invoiceDate.setValue(new Date());
+    this.editBillAccountsForm.controls.distributor.setValue({id:0,name:'',phoneNumber:0,email:'',dlno:'',pan:'',state:'',address:'',city:'',gstin:'',pinCode:''});
+    this.editBillAccountsForm.controls.invoiceItems.setValue({id:0,item:{id:0,name:'',description:'',hsn:{id:0,hsnCode:'',description:'',gstRate:0},manfacturer:{id:0,name:''}},pack:'',batchNo:'',mfgDate:new Date(),expDate:new Date(),qty:0,freeItems:0,mrp:0,rate:0,discount:0});
+    this.editBillAccountsForm.controls.amount.setValue(0);
+    this.editBillAccountsForm.controls.totalDiscount.setValue(0);
+    this.editBillAccountsForm.controls.actualAmount.setValue(0);
+    
+    this.getDistributorsList();
+
+    if(this.data) {
+      this.data.bill.invoiceItems.map(x=>{this.selectedItems.push(x.item)});
+      this.editBill();
+    }
+  }
+
+  getDistributorsList() {
+    this.distributorService.getDistributors().subscribe(
+      res => {
+        this.distributors = res;
+        this.filterSearchDistributors(res);
+      }
+    )
+  }
+
+  filterSearchDistributors(res: Distributor[]) {
+    this.filteredDistributorOptions = this.editBillAccountsForm.controls.distributor.controls.name.valueChanges.pipe(
+      startWith(''),
+      map(term => {
+        return res
+          .map(option => option.name)
+          .filter(option => option.toLowerCase().includes(term as string));
+      },)
+    )
+  }
+
+  onSelectDistributor(option: string) {
+    const distributor = this.distributors.filter(item => item.name === option)[0];
+    this.editBillAccountsForm.controls.distributor.setValue(distributor);
+  }
+
+  searchItems(e: any) {
+    const searchTerm = e.target.value;
+    if(searchTerm.length >= 3) {
+      this.itemsService.getItemByName(searchTerm).subscribe(
+        res => {
+          this.items = res;
+          this.filterSearchItems(res);
+        }
+      )
+    }
+  }
+
+  filterSearchItems(res: Item[]) {
+    this.filteredItemOptions = this.editBillAccountsForm.controls.invoiceItems.controls.item.valueChanges.pipe(
+      startWith(''),
+      map(term => {
+        return res
+          .map(option => option.name)
+          .filter(option => option.toLowerCase().includes(term as string));
+        },
+      )
+    )
+  }
+
+  onSelectItem(option: string, index: number) {
+    const item = this.items.filter(item => item.name === option)[0];
+    const productName = this.items.filter(item => item.name === option)[0].name;
+
+    this.selectedItems.push(item);
+
+    this.editBillForm.controls.billRows.at(index).controls.productName.setValue(productName);
+
+    if(this.data) {
+      this.selectedItems.splice(index,1,item);
+    }
+  }
+
+  /* Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    return this.selection.selected.length === this.billDatasource.data.length;
+  }
+
+  /* Selects all rows if they are not all selected; otherwise clear selection. */
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+    this.selection.select(...this.billDatasource.data.map(item => Object(item).value));
+  }
+
+  /* The label for the checkbox on the passed row */
+  checkboxLabel(row?: BillRows): string {
+    if(!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id}`;
+  }  
+
+  initiateBillForm(): FormGroup {
+    /* initialize a blank row */
+    return new FormGroup({
+      id: new FormControl(),
+      productName: new FormControl(),
+      qty: new FormControl(), 
+      batchNo: new FormControl(), 
+      discount: new FormControl(), 
+      mrp: new FormControl(), 
+      mfgDate: new FormControl(), 
+      expDate: new FormControl(), 
+    })
+  }
+
+  patchBillRows(x: BillRows): FormGroup {
+    return new FormGroup({
+      id: new FormControl(x.id),
+      productName: new FormControl(x.productName),
+      qty: new FormControl(x.qty), 
+      batchNo: new FormControl(x.batchNo), 
+      discount: new FormControl(x.discount), 
+      mrp: new FormControl(x.mrp), 
+      mfgDate: new FormControl(x.mfgDate), 
+      expDate: new FormControl(x.expDate), 
+    })
+  }
+
+  addNewRow() {
+    const control = this.editBillForm.get('billRows') as FormArray;
+    /* Add new blank row below the last filled row */
+    control.push(this.initiateBillForm());
+    this.billDatasource = new MatTableDataSource(control.controls);
+  }
+
+  removeRows() {
+    this.selection.selected.forEach(item => {
+      let index: number = this.billDatasource.data.findIndex(d => d.value === item);
+      this.billDatasource.data.splice(index, 1);
+      this.billDatasource = new MatTableDataSource(this.billDatasource.data);
+    });
+    this.selection =  new SelectionModel<BillRows>(true, []);
+  }
+
+  createFinalObject() {
+    let billRow: any[] = [];
+    
+    for (var i=0; i<this.editBillForm.controls.billRows.value.length; i++){
+      billRow.push({
+        id: this.editBillForm.controls.billRows.value[i].id,
+        item: this.selectedItems[i],
+        batchNo: this.editBillForm.controls.billRows.value[i].batchNo,
+        mfgDate: this.editBillForm.controls.billRows.value[i].mfgDate,
+        expDate: this.editBillForm.controls.billRows.value[i].expDate,
+        qty: this.editBillForm.controls.billRows.value[i].qty as number,
+        discount: this.editBillForm.controls.billRows.value[i].discount as number,
+        mrp: this.editBillForm.controls.billRows.value[i].mrp as number,
+      })
+    }
+
+    const finalObject = {
+      id: this.editBillAccountsForm.value.id,
+      invoiceNumber: this.editBillAccountsForm.value.invoiceNumber,
+      invoiceDate: this.editBillAccountsForm.value.invoiceDate,
+      distributor: this.editBillAccountsForm.value.distributor,
+      invoiceItems: billRow as InvoiceItems[],
+      amount: this.editBillAccountsForm.value.amount,
+      totalDiscount: this.editBillAccountsForm.value.totalDiscount,
+      actualAmount: this.editBillAccountsForm.value.actualAmount
+    } as Invoice
+
+    return finalObject;
+  }
+
+  saveBill() {
+    if(!this.data) {
+      /* set ids of invoiceItems to 0 to post to database */ 
+      this.createFinalObject().invoiceItems.map(x=>{x.id = 0});
+      this.invoiceService.saveInvoice(this.createFinalObject());
+    } else {
+      this.invoiceService.updateInvoice(this.createFinalObject().id, this.createFinalObject());
+    }
+  }
+
+  editBill() {
+    this.editBillForm.controls.billRows.patchValue(this.data.billRows);
+
+    this.editBillAccountsForm.patchValue({
+      id: this.data.bill.id, 
+      invoiceNumber: this.data.bill.invoiceNumber,
+      invoiceDate: this.data.bill.invoiceDate,
+      distributor: this.data.bill.distributor, 
+      amount: this.data.bill.amount,
+      totalDiscount: this.data.bill.totalDiscount,
+      actualAmount: this.data.bill.actualAmount,
+    })
+
+    const control = this.editBillForm.get('billRows') as FormArray;
+    /* Add new blank row below the last filled row */
+    this.data.billRows.map(x=>(
+      control.push(this.patchBillRows(x))
+    ));
+    this.billDatasource = new MatTableDataSource(control.controls);
+  }
+
+  generatePDF() {
+    DocGenBill(this.createFinalObject());
+  }
 }
