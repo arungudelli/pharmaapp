e7031abfef0eeaec755ca1bdb167984ca0ded838
zvdas <rodriguezzude@gmail.com>
2022-12-12 17:59:33 +0530

    new model added for bill rows

---
+++ src/app/models/billRows.ts
@@ -0,0 +1,10 @@
+export interface BillRows {
+    id: number, 
+    productName: string,
+    qty: number, 
+    batchNo: string, 
+    discount: number, 
+    mrp: number, 
+    mfgDate: Date, 
+    expDate: Date, 
+}
